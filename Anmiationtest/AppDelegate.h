42f5c9e86e38ec3531c36bf17b2f206127106719
fitouch666 <fitouch@fitouch666deMacBook-Pro.local>
2017-07-04 16:03:13 +0800

    Initial Commit

---
+++ Anmiationtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Anmiationtest
+//
+//  Created by fitouch666 on 2017/7/4.
+//  Copyright © 2017年 fitouch666. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Anmiationtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Anmiationtest
+//
+//  Created by fitouch666 on 2017/7/4.
+//  Copyright © 2017年 fitouch666. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
